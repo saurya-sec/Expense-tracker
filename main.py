c4b34f70b937cbc890ad6cbede2ebb0c7c83ad10
saurya-sec <saurya456@gmail.com>
2026-08-27 09:54:52 -0700

    database connected

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Depends
+from psycopg import Connection
+
+from database import get_db
 
 app = FastAPI()
 
 
+
+
+
+@app.get("/health/db")
+def check_database(db: Connection = Depends(get_db)):
+
+    with db.cursor() as cursor:
+        cursor.execute("SELECT 1")
+        result = cursor.fetchone()
+
+    return {
+        "status": "ok",
+        "database": "connected",
+        "test": result[0],
+    }
 
 @app.get("/")
 def root():
