3f6f5c60836f8d9123d32e40b810c20d4e810eda
saurya-sec <saurya456@gmail.com>
2026-09-06 07:53:42 -0700

    frontend 2.0

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://expense-tracker-saurya1.vercel.app/"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
